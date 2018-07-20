93ecce33c5f34698a40631b19fbabe90cff228dc
megane9988 <info@m-g-n.me>
2018-07-20 09:28:09 +0900

    ナビゲーションの追加 #14

--- js/app.js
+++ js/app.js
@@ -1,24 +1,27 @@
-// $('#gnav__toggle').click(function () {
-//   $("#gnav__inner").slideToggle();
-// })
-//
-//
-// $(function() {
-//   var $win = $(window),
-//     $main = $('#gnav'),
-//     $nav = $('#gnav__inner'),
-//     navHeight = $nav.outerHeight(),
-//     navPos = $nav.offset().top,
-//     fixedClass = 'is-fixed';
-//
-//   $win.on('load scroll', function() {
-//     var value = $(this).scrollTop();
-//     if ( value > navPos ) {
-//       $nav.addClass(fixedClass);
-//       $main.css('margin-top', navHeight);
-//     } else {
-//       $nav.removeClass(fixedClass);
-//       $main.css('margin-top', '0');
-//     }
-//   });
-// });
+$('#gnav__toggle').click(function () {
+  $("#gnav__inner").slideToggle();
+})
+
+$('#gnav .gnav_item').click(function () {
+    $("#gnav__inner").slideToggle();
+})
+
+$(function() {
+  var $win = $(window),
+    $main = $('#gnav'),
+    $nav = $('#gnav__inner'),
+    navHeight = $nav.outerHeight(),
+    navPos = $nav.offset().top,
+    fixedClass = 'is-fixed';
+
+  $win.on('load scroll', function() {
+    var value = $(this).scrollTop();
+    if ( value > navPos ) {
+      $nav.addClass(fixedClass);
+      $main.css('margin-top', navHeight);
+    } else {
+      $nav.removeClass(fixedClass);
+      $main.css('margin-top', '0');
+    }
+  });
+});
